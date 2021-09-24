6d9bd1bc0923e956155b28cc95691d88c5266fe8
Jhalak Gupta <jhalakguptasps@gmail.com>
2021-09-24 10:06:33 +0530

    Initial Commit

--- app/src/main/java/com/example/androidbootcamp/exercise2/Q5.kt
+++ app/src/main/java/com/example/androidbootcamp/exercise2/Q5.kt
@@ -4,7 +4,7 @@ class Q5 {
     fun getGradeDetails(marks: Int): String {
         println("Your marks is $marks")
         return when(marks) {
-            in 90..100 -> "WellDone"
+            in 90..100 -> "Rockstar"
             in 80..90 -> "Excellent"
             in 70..80 -> "Very Good"
             in 60..70 -> "Good"
