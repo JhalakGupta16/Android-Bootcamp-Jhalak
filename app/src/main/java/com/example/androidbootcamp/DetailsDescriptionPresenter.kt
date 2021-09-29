1740a48d7055588ea7f126dec66836297caa8018
Jhalak Gupta <jhalakguptasps@gmail.com>
2021-09-29 21:44:42 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/androidbootcamp/DetailsDescriptionPresenter.kt
@@ -0,0 +1,18 @@
+package com.example.androidbootcamp
+
+import androidx.leanback.widget.AbstractDetailsDescriptionPresenter
+import androidx.leanback.widget.AbstractDetailsDescriptionPresenter.*
+
+class DetailsDescriptionPresenter : AbstractDetailsDescriptionPresenter() {
+
+    override fun onBindDescription(
+        viewHolder: ViewHolder,
+        item: Any
+    ) {
+        val movie = item as Movie
+
+        viewHolder.title.text = movie.title
+        viewHolder.subtitle.text = movie.studio
+        viewHolder.body.text = movie.description
+    }
+}
